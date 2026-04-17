061f1c04b63ed7a23a59488f9fa346346ce1fc49
Hfcvhhsc123 <mmmmhhuffvi@gmail.com>
2026-04-17 08:50:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors()); 
 
 // مفتاح الـ API الخاص بك الذي استخرجناه بنجاح
-const genAI = new GoogleGenerativeAI(".Ab8RN6KpLdFXYzZuogbEPR2miRZmJvARwNHJQfFRnxE7Z8x-pA");
+const genAI = new GoogleGenerativeAI("AQAb8RN6KpLdFXYzZuogbEPR2miRZmJvARwNHJQfFRnxE7Z8x-pA");
 // رابط للاختبار: افتح الرابط في المتصفح لتتأكد أن السيرفر يعمل
 app.get('/', (req, res) => {
     res.send("Proxy Server is Running!");
